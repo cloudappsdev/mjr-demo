425a93f33fd5fd7defc02b007308cc351abba317
cloudappsdev <webguynow@yahoo.com>
2024-02-19 05:36:13 -0600

    add in Galway Set2 json info

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,78 +1,80 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./Xclips.css";
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import "./Xclips.css"
+import ghfSet1 from "./galway1.json";
+import ghfSet2 from "./galway2.json";
+import ClipSetInfo from "./ClipSetInfo";
 
-import goSet from "./galway1.json";
-import ClipSetInfo from './ClipSetInfo';
-
-const goSetX: ClipData[] = goSet;
-
-
+const ghfxSet1: ClipData[] = ghfSet1;
+const ghfxSet2: ClipData[] = ghfSet2;
 
 function Home() {
-
-  console.log('goSet', goSetX);
-
-/*   console.log('');
-  console.log('typeof goSetX', typeof goSetX);
-
-
-  console.log("typeof set1Ord: ", typeof set1Ord);
-  console.log('');
-  console.log('typeof set1Ord.clipInfo:', typeof set1Ord.clipInfo);
- */
-
   return (
     <>
-    <div>my-header</div>
-    <Tabs>
-    <TabList style={{ width: "50%", margin: "0 auto" }}>
-      <Tab style={{ width: "45%"}}>Acoustic</Tab>
-      <Tab style={{ width: "45%"}}>Full-Band</Tab>
-    </TabList>
-
-    <TabPanel>
-      <br/>
+      <div>my-header</div>
+      <div>
+        Our inept "set-it and forget-it" videographer, didn't always capture the
+        whole stage or players, but we hope you enjoy the Music while you are
+        here!
+      </div>
       <Tabs>
         <TabList style={{ width: "50%", margin: "0 auto" }}>
-          <Tab style={{ width: "45%"}}>Set 1</Tab>
-          <Tab style={{ width: "45%"}}>Set 2</Tab>
+          <Tab style={{ width: "45%" }}>Acoustic</Tab>
+          <Tab style={{ width: "45%" }}>Full-Band</Tab>
         </TabList>
 
         <TabPanel>
-          
-        </TabPanel>
-        <TabPanel>
-          
-        </TabPanel>
-      </Tabs>
-      
-    </TabPanel>
-    <TabPanel>
-      <br/>
-      <Tabs className="cmg">
+          <br />
+          <Tabs>
             <TabList style={{ width: "50%", margin: "0 auto" }}>
-              <Tab style={{ width: "45%"}}>Set 1x</Tab>
-              <Tab style={{ width: "45%"}}>Set 2</Tab>
+              <Tab style={{ width: "45%" }}>Set 1</Tab>
+              <Tab style={{ width: "45%" }}>Set 2</Tab>
             </TabList>
 
             <TabPanel>
-              <ClipSetInfo clipInfo={goSetX} />
-              
+              <iframe
+                width="987"
+                height="555"
+                src="https://www.youtube.com/embed/80m6ENmR45k"
+                title="Mural Uno Mix Sampler"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
             </TabPanel>
             <TabPanel>
-              
+              <iframe
+                width="987"
+                height="555"
+                src="https://www.youtube.com/embed/G60jnMDTBLI"
+                title="Mix2 of Mural Art Fest, The MarJays"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
             </TabPanel>
+          </Tabs>
+        </TabPanel>
+        <TabPanel>
+          <br />
+          <Tabs className="cmg">
+            <TabList style={{ width: "50%", margin: "0 auto" }}>
+              <Tab style={{ width: "45%" }}>Set 1x</Tab>
+              <Tab style={{ width: "45%" }}>Set 2</Tab>
+            </TabList>
+
+            <TabPanel>
+              <ClipSetInfo clipInfo={ghfxSet1} />
+            </TabPanel>
+            <TabPanel>
+              <ClipSetInfo clipInfo={ghfxSet2} />
+            </TabPanel>
+          </Tabs>
+        </TabPanel>
       </Tabs>
-      
+    </>
+  );
+}
 
-
-    </TabPanel>
-  </Tabs>
-  </>
-  )
-  }
-
-
-export default Home
+export default Home;
